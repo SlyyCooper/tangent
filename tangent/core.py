bc0d6b21d69ed5268efbce2e2f09f1b95664292d
SlyyCooper <trevornichols94@gmail.com>
2024-12-29 17:11:31 -0500

    Revert "Update core.py"
    This reverts commit ad4a8a1bfde71616842234629a17c3521e605c37.

--- tangent/core.py
+++ tangent/core.py
@@ -23,7 +23,7 @@ from .types import (
 __CTX_VARS_NAME__ = "context_variables"
 
 
-class Tangent:
+class tangent:
     def __init__(self, client=None):
         if not client:
             client = OpenAI()
