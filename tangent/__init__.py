2b990c98a8d9a7f69185c15a79775913a4d6468e
SlyyCooper <trevornichols94@gmail.com>
2024-12-29 17:09:38 -0500

    Update __init__.py

--- tangent/__init__.py
+++ tangent/__init__.py
@@ -1,4 +1,4 @@
-from .core import Tangent
-from .types import Agent, Response, Result
+from tangent.core import Tangent
+from tangent.types import Agent, Response, Result
 
 __all__ = ["Tangent", "Agent", "Response", "Result"]
